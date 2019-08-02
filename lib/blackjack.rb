370780850bd7b0808a2d81315ef6ebb7fb300d39
Will Bush <williamdcbush@gmail.com>
2019-08-02 08:39:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,11 +53,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
   welcome
   card_total = initial_round
-  
-  hit?
-  display_card_total
+  until card_total > 21 
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total) 
 end
     
